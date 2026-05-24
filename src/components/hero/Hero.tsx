efc0c21cb72aa952303c5f97ef4f4246544d767d
Pernilla <78024835+pernillafed@users.noreply.github.com>
2026-05-24 14:59:59 +0200

    replace image

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,24 +1,34 @@
 import styles from "./Hero.module.css";
-import MyPhoto from "../../assets/IMG_4323.jpg";
+import MyPhoto from "../../assets/MyPhoto.jpeg";
 
 const Hero = () => {
-    return (
-        <div className={styles.hero}>
-            <div className={styles.heroDescription}>
-                <p className={styles.shortInfo}>28th of August 1991 | Humanistgatan 8A in Malmö</p>
-                <div className={styles.contact}>
-                    <h1 className={styles.contactHeading}>Contact</h1>
-                    <p>+46 76 555 58 34</p>
-                    <a
-                        href="mailto:pernilla.lundahl@hotmail.com"
-                        className={styles.contactLink}
-                    >pernilla.lundahl@hotmail.com</a>
-                    <a target="_blank" href="https://www.linkedin.com/in/pernilla-lundahl-80b391244" className={styles.contactLink}>LinkedIn profile</a>
-                </div>
-            </div>
-            <img src={MyPhoto} alt="Pernilla Lundahl" className={styles.heroPhoto} />
+  return (
+    <div className={styles.hero}>
+      <div className={styles.heroDescription}>
+        <p className={styles.shortInfo}>
+          28th of August 1991 | Humanistgatan 8A in Malmö
+        </p>
+        <div className={styles.contact}>
+          <h1 className={styles.contactHeading}>Contact</h1>
+          <p>+46 76 555 58 34</p>
+          <a
+            href="mailto:pernilla.lundahl@hotmail.com"
+            className={styles.contactLink}
+          >
+            pernilla.lundahl@hotmail.com
+          </a>
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/pernilla-lundahl-80b391244"
+            className={styles.contactLink}
+          >
+            LinkedIn profile
+          </a>
         </div>
-    );
-}
- 
+      </div>
+      <img src={MyPhoto} alt="Pernilla Lundahl" className={styles.heroPhoto} />
+    </div>
+  );
+};
+
 export default Hero;
